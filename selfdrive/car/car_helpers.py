1cd6a41650253ea2bded4f8ff98ef919c2650b5c
alfhern <hz.alfonso@gmail.com>
2020-12-17 11:46:15 +0100

    hardcode fingerprint for totyot

--- selfdrive/car/car_helpers.py
+++ selfdrive/car/car_helpers.py
@@ -82,8 +82,8 @@ def only_toyota_left(candidate_cars):
 
 # **** for use live only ****
 def fingerprint(logcan, sendcan):
-  fixed_fingerprint = os.environ.get('FINGERPRINT', "")
-  skip_fw_query = os.environ.get('SKIP_FW_QUERY', False)
+  fixed_fingerprint = 'TOYOTA RAV4 HYBRID 2019'  # os.environ.get('FINGERPRINT', "")
+  skip_fw_query = True  # os.environ.get('SKIP_FW_QUERY', False)
 
   if not fixed_fingerprint and not skip_fw_query:
     # Vin query only reliably works thorugh OBDII
